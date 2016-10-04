508bb2f4731b5a023892bec14322f09a9505b7a1
Scott Kirkland <srkirkland@gmail.com>
2016-10-04 16:06:53 -0700

    add in auth token filter

---
+++ ReportService/Attributes/AuthTokenFilter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace ReportService.Attributes
+{
+    public class AuthTokenFilter : System.Web.Http.Filters.AuthorizationFilterAttribute
+    {
+        private static readonly string ValidToken = WebConfigurationManager.AppSettings["token"];
+
+        /// <summary>
+        /// Look in the X-Auth-Token header and also the params for a token, and then check it against the proper value
+        /// </summary>
+        /// <param name="actionContext"></param>
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            //First check the preferred token header
+            var tokenHeader =
+                actionContext.Request.Headers.SingleOrDefault(
+                    h => string.Equals(h.Key, "X-Auth-Token", StringComparison.InvariantCultureIgnoreCase));
+
+            if (tokenHeader.Value != null &&
+                string.Equals(tokenHeader.Value.First(), ValidToken, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return;
+            }
+
+            //Else try the query string token
+            var query = HttpUtility.ParseQueryString(actionContext.Request.RequestUri.Query);
+
+            if (string.Equals(query["token"], ValidToken, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return;
+            }
+
+            //Nothing is valid, so reject
+            throw new HttpResponseException(HttpStatusCode.Unauthorized);
+        }
+    }
+}
