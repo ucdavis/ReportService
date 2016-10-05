de1031041dcea6100ac24bc369751ec32e3ab210
Scott Kirkland <srkirkland@gmail.com>
2016-10-05 10:23:20 -0700

    remove same, ready for use

--- ReportService/Controllers/HomeController.cs
+++ ReportService/Controllers/HomeController.cs
@@ -16,18 +16,5 @@ namespace ReportService.Controllers
 
             return View();
         }
-
-        public async Task<ActionResult> Excel()
-        {
-            string token = WebConfigurationManager.AppSettings["token"];
-            const string data = "{\"CollegeId\":\"AE\",\"TermCode\":\"201601\",\"Honors4590\":3.725,\"HighHonors4590\":3.838,\"HighestHonors4590\":3.894,\"Honors90135\":3.634,\"HighHonors90135\":3.774,\"HighestHonors90135\":3.872,\"Honors135\":3.546,\"HighHonors135\":3.725,\"HighestHonors135\":3.838}\"";
-
-            var client = new HttpClient();
-            var result = await client.PostAsync("https://test.caes.ucdavis.edu/ReportService/api/honors?token=" + token, new StringContent(data, Encoding.UTF8, "application/json"));
-
-            var contents = await result.Content.ReadAsByteArrayAsync();
-
-            return File(contents, "application/excel", "Honors.xls");
-        }
     }
 }
